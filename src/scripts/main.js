87d2d696654753efb58b07d90911d2966a11646f
George Kelley <geokelley@gmail.com>
2019-09-12 10:32:25 -0500

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,13 @@
+// Lightning Exercise 1: Write a factory function that creates an object that represents a doctor. The function should accept three arguments.
+//     Doctor's name
+//     Specialty (Oncologist, pediatrician, etc...)
+//     Address of practice
+
+const createDoctorObject = (name, specialty, addressOfPractice,) => {
+    return {
+        name: name.name,
+        specialty: specialty.specialty,
+        addressOfPractice: addressOfPractice.addressOfPractice,
+      
+    }
+}
