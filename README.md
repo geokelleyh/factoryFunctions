05a6c5f7277593011b0a2bc380e68a9fd72959c9
George Kelley <geokelley@gmail.com>
2019-09-13 10:48:09 -0500

    saving work so nothing is lost

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # factoryFunctions
 Work in progress....to be continued
+
+update:  Fauty code, still work in progress.  
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -34,21 +34,21 @@ const createPet = (petName, petBreed) => {
 
 }
 
-        const fluffy = createPet("Fluffy", "Rat Terrier")
-         
-        const BowWowKennels = []
-                 BowWowKennels.push(fluffy)
-             console.log(fluffy)
+const fluffy = createPet("Fluffy", "Rat Terrier")
+
+const BowWowKennels = []
+BowWowKennels.push(fluffy)
+console.log(fluffy)
 
 
-        const groucho = createPet("Groucho", "Rat Terrier")
-            console.log(groucho)
-                 BowWowKennels.push(groucho)
+const groucho = createPet("Groucho", "Rat Terrier")
+console.log(groucho)
+BowWowKennels.push(groucho)
 
-        const poohBear = createPet("PoohBear", "Rat Terrier")
-            console.log(poohBear)
-                BowWowKennels.push(poohBear)
-            console.log(BowWowKennels)
+const poohBear = createPet("PoohBear", "Rat Terrier")
+console.log(poohBear)
+BowWowKennels.push(poohBear)
+console.log(BowWowKennels)
 
 //             Practice: Music Row
 // Your job is to sign each of these promising young music stars to the appropriate label.
@@ -69,28 +69,66 @@ const createPet = (petName, petBreed) => {
 // Austin Kinkaid is a Pop artist and is 22 years old
 // Loyoncé Branis is a Rap artist and is 27 years ol
 
-const createArtistProfile= {
-    name,
-    genre,
-    age,
-
+const createArtistProfile = ( name, genre, age)  => {
+    return {
+        name,
+        genre,
+        age,
+    }
 }
-const jumpStopRecords = [];
-const chattenRecords = [];
-const polarRecords = [];
+const jumpStopRecordsRap = [];
+const chattenRecordsCountry = [];
+const polarRecordsPop = [];
 
 const artistsArray = []
 
-const bruceAtikins = createArtistProfile( "Bruce Atikins","Country", 23)
-console.log(bruceAtikins)
+const bruceAtikins = createArtistProfile("Bruce Atikins", "Country", 23)
+console.log("profile of",bruceAtikins)
+artistsArray.push(bruceAtikins)
+console.log("first run of array",artistsArray)
 
-const jensenBrown = createArtistProfile("Jensen Brown","Pop", 20)
-console.log(jensenBrown)
+const jensenBrown = createArtistProfile("Jensen Brown", "Pop", 20)
+console.log("profile of",jensenBrown)
+artistsArray.push(jensenBrown)
+console.log("second run of array",artistsArray)
 
 const dreFunkz = createArtistProfile("DreFunkz", "Rap", 25)
-console.log(dreFunkz)
+console.log("profile of",dreFunkz)
+artistsArray.push(dreFunkz)
+console.log("third run of",artistsArray)
 
 const dustaGrimes = createArtistProfile("Dusta Grimes", "Rap", 21)
-console.log(dustaGrimes)
+console.log("profile of",dustaGrimes)
+artistsArray.push(dustaGrimes)
+console.log("fourth run of ",artistsArray)
 
-const
+
+const barthDanielson = createArtistProfile("Bartholomew Danielson", "Bluegrass", 23)
+console.log("Profile of",barthDanielson)
+artistsArray.push(barthDanielson)
+console.log("fifth run of ",artistsArray)
+
+const avilDallas = createArtistProfile("Avilee Dallas", "Country", 19)
+console.log("profile of",avilDallas)
+artistsArray.push(avilDallas)
+console.log("sixth run of ",artistsArray)
+
+const austinKink = createArtistProfile("Austin Kinkaid", "Pop", 22)
+console.log("profile of",austinKink)
+artistsArray.push(austinKink)
+console.log("seventh run of ",artistsArray)
+
+
+const loyBranis = createArtistProfile("Loyoncé Branis", "Rap", 27)
+console.log("profile of",loyBranis)
+artistsArray.push(loyBranis)
+console.log("eighth and final run of",artistsArray)
+
+// Not sure why the array completely populated on each run?
+
+
+
+artistsArray.forEach( let i = artistsArray.genre[country], i < artistsArray.length,i++) =>
+ { chattenRecordsCountry.push(artistsArray.genre[country]
+    
+});
